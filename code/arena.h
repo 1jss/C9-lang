448fa8c247f7a0e2efa393f56998b604184ba605
1jss <62594271+1jss@users.noreply.github.com>
2024-05-31 08:30:00 +0200

    Split c9 header into separate files
    - Arena is now a separate header
    - c9 types is now moved to types.h

--- code/arena.h
+++ code/arena.h
@@ -1,41 +1,5 @@
 #include <inttypes.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdio.h>
-
-typedef int32_t i32;
-typedef int64_t i64;
-typedef uint32_t u32;
-typedef uint64_t u64;
-typedef float f32;
-typedef double f64;
-
-typedef struct {
-  uint8_t *data;
-  size_t len;
-} t8;
-
-t8 text(char *s) {
-  uint32_t len = 0;
-  while (s[len] != '\0') {
-    len++;
-  }
-  return (t8){(uint8_t *)s, len};
-}
-
-void print_i32(i32 value) { printf("%" PRId32, value); }
-void print_i64(i64 value) { printf("%" PRId64, value); }
-void print_u32(u32 value) { printf("%" PRIu32, value); }
-void print_u64(u64 value) { printf("%" PRIu64, value); }
-void print_f32(f32 value) { printf("%f", value); }
-void print_f64(f64 value) { printf("%f", value); }
-void print_bool(bool value) { printf("%d", value); }
-void print_string(char *value) { printf("%s", value); }
-void print_text(t8 value) {
-  for (size_t i = 0; i < value.len; i++) {
-    printf("%c", value.data[i]);
-  }
-}
+#include <stdlib.h>
 
 // Simple arena allocator that has the following functions:
 // - a_open: initializes the arena and returns a pointer to it
---
+++ code/types.h
@@ -0,0 +1,38 @@
+#include <inttypes.h> // uint8_t, int32_t, int64_t, uint32_t, uint64_t
+#include <stdbool.h> // bool
+#include <stddef.h> // size_t
+#include <stdio.h> // printf
+
+typedef int32_t i32;
+typedef int64_t i64;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef float f32;
+typedef double f64;
+
+typedef struct {
+  uint8_t *data;
+  size_t len;
+} t8;
+
+t8 text(char *s) {
+  uint32_t len = 0;
+  while (s[len] != '\0') {
+    len++;
+  }
+  return (t8){(uint8_t *)s, len};
+}
+
+void print_i32(i32 value) { printf("%" PRId32, value); }
+void print_i64(i64 value) { printf("%" PRId64, value); }
+void print_u32(u32 value) { printf("%" PRIu32, value); }
+void print_u64(u64 value) { printf("%" PRIu64, value); }
+void print_f32(f32 value) { printf("%f", value); }
+void print_f64(f64 value) { printf("%f", value); }
+void print_bool(bool value) { printf("%d", value); }
+void print_string(char *value) { printf("%s", value); }
+void print_text(t8 value) {
+  for (size_t i = 0; i < value.len; i++) {
+    printf("%c", value.data[i]);
+  }
+}
