b2b8dcf0c6bb097832dc0c123e3a339a640fd399
1jss <62594271+1jss@users.noreply.github.com>
2024-06-05 08:03:08 +0200

    Documentation of array lib
    Added allocation fail guards

--- code/array.h
+++ code/array.h
@@ -6,15 +6,41 @@
 
 #include "arena.h" // Arena, arena_open, arena_fill, arena_close
 
-// Dynamic array implementation that has the following functions:
-// - array_create: initializes the array and returns a pointer to it
-// - array_destroy: frees all memory in the array
-// - array_push: adds an element to the array
-// - array_pop: removes the last element from the array
-// - array_get: returns the element at the given index
-// - array_set: sets the element at the given index
-// - array_length: returns the used size of the array
-// - array_last: returns the last index of the array
+#if 0
+Dynamic array implementation that has the following functions:
+ - array_create: initializes the array and returns a pointer to it
+ - array_destroy: frees all memory in the array
+ - array_push: adds an element to the array
+ - array_pop: removes the last element from the array
+ - array_get: returns the element at the given index
+ - array_set: sets the element at the given index
+ - array_length: returns the used size of the array
+ - array_last: returns the last index of the array
+
+The array index is implemented around a tree structure to allow for fast access and insertion. Each layer of the tree has a width of index_width and the depth is determined by the number of items in the array.
+
+Consider an example where 'index_width' is 10. The tree would look something like this:
+
+Layer 1: 0 1 2 3 4 5 6 7 8 9
+Each of these nodes can have up to 10 children:
+
+Layer 2: 0 1 2 3 4 5 6 7 8 9 (for each of the nodes in layer 1)
+And each of those nodes can have up to 10 children, and so on:
+
+Layer 3: 0 1 2 3 4 5 6 7 8 9 (for each of the nodes in layer 2)
+
+The indexes are stored in reverse order, meaning that the largest indexes are deepest into the tree.
+
+For example:
+- The index 198 would be stored at positions 8 -> 9- > 1 in layers 1, 2, and 3
+- The index 42 would be stored at positions 2 -> 4 in layers 1 and 2
+- The index 1024 would be stored at positions 4 -> 2 -> 0 -> 1 in layers 1, 2, 3, and 4
+
+The tree structure grows as more items are added to the array, adding more layers as needed.
+
+The array index is stored in an arena allocator to allow for fast allocation and growth without needing to free memory on every pop or set operation. All items added to the array are copied to the array's arena, so the original data can be safely disposed of after adding it to the array.
+
+#endif
 
 const size_t DEFAULT_INDEX_WIDTH = 16;
 
@@ -33,7 +59,6 @@ typedef struct {
 
 // Create a new index node and return a pointer to it
 IndexNode *index_create(Arena *arena) {
-  // printf("- array.h | Adding IndexNode: %zu\n", sizeof(IndexNode));
   IndexNode *index = (IndexNode *)arena_fill(arena, sizeof(IndexNode));
   index->children = 0;
   index->item = 0;
@@ -133,8 +158,8 @@ Array *array_create(Arena *arena) {
 
 // Copy data onto the array and add it to the last position
 void array_push(Array *array, void *data, size_t data_size) {
-  // printf("- array.h | Adding data: %zu\n", data_size);
   void *item = arena_fill(array->arena, data_size);
+  if (item == 0) return; // Allocation failed
   memcpy(item, data, data_size);
 
   // Add the item to the index
@@ -151,9 +176,7 @@ void array_push(Array *array, void *data, size_t data_size) {
 
 // Get the data from the last item and remove it from the array
 void *array_pop(Array *array) {
-  if (array->length == 0) {
-    return 0;
-  }
+  if (array->length == 0) return 0; // No items to pop
   array->length -= 1;
   // Get last item and remove it from the index
   index_get_params get_params = {
@@ -175,9 +198,7 @@ void *array_pop(Array *array) {
 
 // Get the data at the given index starting from 0
 void *array_get(Array *array, size_t index) {
-  if (index >= array->length) {
-    return 0;
-  }
+  if (index >= array->length) return 0;
   index_get_params get_params = {
     .indexNode = array->index,
     .index = index,
@@ -188,10 +209,9 @@ void *array_get(Array *array, size_t index) {
 
 // Set the data at the given index starting from 0
 void array_set(Array *array, size_t index, void *data, size_t data_size) {
-  if (index >= array->length) {
-    return;
-  }
+  if (index >= array->length) return;
   void *item = arena_fill(array->arena, data_size);
+  if (item == 0) return; // Allocation failed
   memcpy(item, data, data_size);
   index_set_params set_params = {
     .arena = array->arena,
@@ -211,9 +231,7 @@ size_t array_length(Array *array) {
 // Get last index of the array
 size_t array_last(Array *array) {
   // If the array is empty return -1
-  if (array->length == 0) {
-    return -1;
-  }
+  if (array->length == 0) return -1;
   return array->length - 1;
 }
 
